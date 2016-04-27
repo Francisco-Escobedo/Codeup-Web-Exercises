998e745c3edaaac9834d9eab57331000b404a1ca
Francisco Escobedo <franciscoeescobedo@gmail.com>
2016-04-27 14:30:35 -0500

    Added notes from in-class example

--- public/login.php
+++ public/login.php
@@ -2,6 +2,12 @@
 
 $error = '';
 
+// if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // $username = $_POST['username'];
+    // $password = $_POST['password'];
+// }
+
+
 if (!isset($_POST['username']) && !isset($_POST['password'])){
     $username = '';
     $password = '';
