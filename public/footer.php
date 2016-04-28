7451e18575a0d9b4ba07a07851cbf72ae0a6c7c6
Francisco Escobedo <franciscoeescobedo@gmail.com>
2016-04-28 15:33:48 -0500

    Created header, footer, and navbar templates and applied the templates to a blank html page (inclusion-template). Utilized require to add the information to the page.

---
+++ public/footer.php
@@ -0,0 +1,5 @@
+<div data-role="footer" data-position="fixed">
+    <h1>Footer goes here</h1>
+</div>
+
+</div>
---
+++ public/header.php
@@ -0,0 +1,4 @@
+<div data-role="page">
+  <div data-role="header" data-position="fixed">
+    <h1>Header Goes Here</h1>
+  </div>
