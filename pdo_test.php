2d134aeb50c82d7e2946627f30919ed21ca14a0d
Francisco Escobedo <franciscoeescobedo@gmail.com>
2016-05-12 10:14:03 -0500

    Added credentials file requirement

--- pdo_test.php
+++ pdo_test.php
@@ -1,10 +1,5 @@
 <?php 
 
-DEFINE ('DB_HOST', '127.0.0.1');
-DEFINE ('DB_NAME', 'employees');
-DEFINE ('DB_USER', 'codeup');
-DEFINE ('DB_PASS', 'modula');
-
 require 'db_connect.php';
 
 echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
