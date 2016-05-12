0e9ca278450f4e3cb5883fc13dab644db69f0505
Francisco Escobedo <franciscoeescobedo@gmail.com>
2016-05-12 09:58:23 -0500

    Stored sensitive data such as DB_HOST, DB_NAME, DB_USER, and DB_PASS into variables.

--- db_connect.php
+++ db_connect.php
@@ -1,5 +1,5 @@
 <?php
 
-$dbc = new PDO('mysql:host=127.0.0.1;dbname=employees', 'codeup', 'modula');
+$dbc = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME,DB_USER,DB_PASS);
 
 $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
