6cc057dfeaf08899bf6fb429d6669bc0c98bda28
Francisco Escobedo <franciscoeescobedo@gmail.com>
2016-05-12 14:07:34 -0500

    Deleted blank space and added message to pdo_test to let user know when successful connection has been made.

--- pdo_test.php
+++ pdo_test.php
@@ -1,5 +1,22 @@
 <?php 
 
+require 'db_credentials.php';
 require 'db_connect.php';
 
 echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
+
+$users = [
+    'luis',
+    'zach',
+    'paul',
+    'sam',
+    'taylor',
+    'sal',
+    'colin'
+];
+
+foreach ($users as $name){
+    $query = 'INSERT INTO users (name) VALUES ("'. $name . '")';
+    $dbc->exec($query);
+    var_dump($dbc->lastInsertId());
+}
