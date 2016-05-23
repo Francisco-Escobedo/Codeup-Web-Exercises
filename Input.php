f1896b6a1b99c7f3205e8c5e2b2c34c5135089ea
Francisco Escobedo <franciscoeescobedo@gmail.com>
2016-05-23 09:36:15 -0500

    Updated getString and getNumer methods to take in two optional parameters.
    If $key is not a string, or $min & $max are not numbers, throw an InvalidArgumentException.
    
    If the requested key is missing from the input, throw an OutOfRangeException
    
    If the value is the wrong type, throw a DomainException
    
    If a string is shorter than $min or longer than $max, throw a LengthException
    
    If a number is less than $min or larger than $max, throw a RangeException.

--- Input.php
+++ Input.php
@@ -1,5 +1,11 @@
 <?php
 
+class InvalidArgumentException extends Exception;
+class OutOfRangeException extends Exception;
+class DomainException extends Exception;
+class LengthException extends Exception;
+class RangeException extends Exception;
+
 class Input
 {
     /**
@@ -28,8 +34,24 @@ class Input
     }
 
 
-    public static function getString($key)
+    public static function getString($key, $min=null, $max=null)
     {
+        if(string($key) || !is_numeric($min) || !is_numeric($max)){
+            throw new InvalidArgumentException ('Invalid entry into a field expecting numeric characters (integers)');
+        }
+
+        if(empty($key)){
+            throw new OutOfRangeException ('Key value is missing from input');
+        }
+
+        if(is_numeric($key)){
+            throw new DomainException ('Value wrong data type');
+        }
+
+        if(strlen($key)<$min || strlen($key)>$max){
+            throw new LengthException ('String shorter or longer than accepted length')
+        }
+
         if(self::get($key)!=null && is_string(self::get($key)) && 
             !is_numeric(self::get($key))){
            return self::get($key);
@@ -39,8 +61,24 @@ class Input
     }
 
 
-    public static function getNumber($key)
+    public static function getNumber($key, $min=null, $max=null)
     {
+        if(string($key) || !is_numeric($min) || !is_numeric($max)){
+            throw new InvalidArgumentException ('Invalid entry into a field expecting numeric characters (integers)');
+        }
+
+        if(empty($key){
+            throw new OutOfRangeException ('Key value is missing from input');
+        }   
+
+        if(!is_numeric($key)){
+            throw new DomainException ('Value wrong data type');
+        }
+
+        if($key<$min || $key>$max){
+            throw new RangeException ('Numeric value smaller or longer than accepted range')
+        }
+
         if(self::get($key)!=null &&
         is_numeric(self::get($key)))
         {
@@ -56,8 +94,6 @@ class Input
     {
         if(self::get($key)!=null && self::get($key)>1000 && self::get($key)<2100){
             return self::get($key);
-            self::$d = new DateTime();
-            // return $d->format(Y);
         } else {
             throw new Exception ('Date established does not exist or is not within acceptable date range');
         }
